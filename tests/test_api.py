00831ac637a86cf6e82b292755dfde6399a95049
unknown <stan.hordon@gmail.com>
2024-03-11 11:47:35 -0700

    upload image doesn't work

--- tests/test_api.py
+++ tests/test_api.py
@@ -24,12 +24,12 @@ def test_create_user():
 
 # upload pet image + pet Id
 def test_upload_pet_image():
-    headers = {"Authorization": api_key}
+    headers = {"Authorization": api_key , "Content-Type": "multipart/form-data"}
     # with open(file_path, "rb") as image_file:
     #     image_data = image_file.read()     
     files = {'image': (open(file_path, 'rb'))}  
     petId = 240787
-    upload_pet_response = requests.post(endpoint + f"/pet/{petId}/uploadImage", headers = headers, files = files, )
+    upload_pet_response = requests.post(endpoint + f"/pet/{petId}/uploadImage", headers = headers, files = files)
     status_code = upload_pet_response.status_code
     # upload_pet_response_data = upload_pet_response.json()
     print(status_code)
@@ -102,33 +102,39 @@ def test_create_list_users():
     print(create_users_response.json())
     assert status_code_create_users == 200
     if status_code_create_users == 200:
-        dog_buyer_user = body[1]
-        user_name = dog_buyer_user["username"]
-        print(user_name)
-        username = user_name
-        user_name_update_body = [
-                                {"id": 1044,
-                                "username": "DOG_LOVER",
-                                "firstName": "Sam",
-                                "lastName": "Deer",
-                                "email": "sam@gmail.com",
-                                "password": "pass34",
-                                "phone": "4353453454",
-                                "userStatus": 0       
-                                }
-                                ]
+        body_login = [
+                {"username": "Catbuytrer",
+                 "password": "passty123"  
+                }
+            ]
+        login_response = requests.get(endpoint + "/user/login", headers=headers, json=body_login)
+        status_code_login = login_response.status_code
+        print(status_code_login)
+        print(login_response.json())
+        
+        if status_code_login == 200:
 
-        update_user_name_response = requests.put(endpoint + f"/user/{username}", headers=headers, json=user_name_update_body)
-        status_code_user_name_update = update_user_name_response.status_code
-        print(status_code_user_name_update)
-        print(username)
-        assert status_code_user_name_update == 200, "ERROR user name update"
+            username = body_login[0]["username"]
+            user_body = {
+                "id": 1046,
+                "username": "NEW_CAT_USER",
+                "firstName": "Marirtyna",
+                "lastName": "Gutrym",
+                "email": "gurtym@gmail.com",
+                "password": "passty123",
+                "phone": "4353453try454",
+                "userStatus": 0       
+                }
+            update_user_name_response = requests.put(endpoint + f"/user/{username}", headers=headers, json=user_body)
+            update_user_name_status_code = update_user_name_response.status_code
+            print(update_user_name_status_code)
+            print(update_user_name_response.json())
     else:
-        print("ERROR user name update")
-
+        print("user creation ERRor")
+    
 # get user
 def test_get_user():
-    get_user_response = requests.get(endpoint + "/user/dog-buyer")
+    get_user_response = requests.get(endpoint + "/user/NEW_CAT_USER")
     status_code_get_user = get_user_response.status_code
     print(status_code_get_user)
     print(get_user_response.json())
